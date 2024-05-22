2baef895c6b30189b6fc7beda5517b602e681fc2
jamog3 <mailtojam@gmail.com>
2024-05-22 11:45:56 +0900

    feat: change path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "../stories/Button";
+import { Button } from "@/components";
 
 export default function Home() {
   return (
